5efd5354e2adc83491c34257d10d9f5130fd746d
Daníel Freyr Grétarsson <danielfgretars@gmail.com>
2022-04-25 09:09:31 +0000

    #1. Added feature to print out username when greeting

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 class HelloWorld:
     @classmethod
-    def hello_word(cls):
-        print("Hello world!")
+    def hello_word(cls, username):
+        print(f"Hello {username}!")
 
 if __name__ == '__main__':
-    HelloWorld.hello_word()
+    username = input("Enter your username: ")
+    HelloWorld.hello_word(username)
