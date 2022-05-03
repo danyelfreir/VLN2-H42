81840a95a1ff5f31603832518adcb14070531510
Daníel Freyr Grétarsson <danielfgretars@gmail.com>
2022-05-03 20:27:18 +0000

    HTML and CSS split up and made a bit more Djangonic

--- code/fyre_sale/frontpage/views.py
+++ code/fyre_sale/frontpage/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'frontpage/frontpage.html')
+    return render(request, 'frontpage/index.html')
