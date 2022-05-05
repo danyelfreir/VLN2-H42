2a876794555dcb7a820885adf790f403cf240655
Daníel Freyr Grétarsson <danielfgretars@gmail.com>
2022-05-05 15:40:54 +0000

    Haldið áfram með endpoints og bras

--- code/fyre_sale/items/urls.py
+++ code/fyre_sale/items/urls.py
@@ -2,8 +2,8 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.items_index, name="items_index")
-    # path('<int:item_id>', views.singleitem.html, name="single_item")
+    path('', views.items_index, name="items_index"),
+    path('item/<int:item_id>', views.single_item, name="single_item")
     # path('all/', views.items_index, name="items_index"),
 ]
 
--- code/fyre_sale/items/views.py
+++ code/fyre_sale/items/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from items.models import ItemForSale
 
 # Create your views here.
@@ -7,5 +8,6 @@ def items_index(request):
         'items': ItemForSale.objects.all().order_by('time_of_upload')
 })
 
-def single_item(request):
-    return render(request, 'items/singleitem.html')
+def single_item(request, item_id):
+    return HttpResponse("Detail on item", item_id)
+    #return reverse(request, 'items/test.html')
