9db34e0a75a68e369026003bed62e1adc206f640
Daníel Freyr Grétarsson <danielfgretars@gmail.com>
2022-05-05 09:17:20 +0000

    Minor  changes to run

--- code/fyre_sale/djangoProject/settings.py
+++ code/fyre_sale/djangoProject/settings.py
@@ -95,12 +95,8 @@ DATABASES = {
     #     'PORT': '5432'
     # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'fyresale_db',
-        'USER': 'postgres',
-        'PASSWORD': 'lol123',
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- code/fyre_sale/items/urls.py
+++ code/fyre_sale/items/urls.py
@@ -2,4 +2,5 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
+    #path('all/', items.items_index, name="items_index"),
 ]
