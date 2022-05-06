0fb610bfe7a980c4a11ba8887bf88f3c7ed42640
Daníel Freyr Grétarsson <danielfgretars@gmail.com>
2022-05-06 14:02:53 +0000

    Byrjað á búa til form

--- code/fyre_sale/users/forms.py
+++ code/fyre_sale/users/forms.py
@@ -1,4 +1,6 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.forms import ModelForm
+from django.contrib.auth.models import User
 
 class SignInForm(AuthenticationForm):
     def __init__(self):
@@ -6,3 +8,15 @@ class SignInForm(AuthenticationForm):
         for field in self.visible_fields():
             field.field.widget.attrs['class'] = 'form-field'
 
+
+class SignUpForm(ModelForm):
+    class Meta:
+        model = User
+        fields = [
+            'email', 'username', 'first_name', 'last_name', 'password',
+        ]
+
+    def __init__(self):
+        super(ModelForm, self).__init__()
+        for field in self.visible_fields():
+            field.field.widget.attrs['class'] = 'form-field'
