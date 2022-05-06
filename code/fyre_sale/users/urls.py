ba004ce5534d36758dba7260e8d426c8afb12a6f
Daníel Freyr Grétarsson <danielfgretars@gmail.com>
2022-05-06 11:02:53 +0000

    Tekið til í static files

--- code/fyre_sale/users/urls.py
+++ code/fyre_sale/users/urls.py
@@ -2,7 +2,8 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('signup', views.sign_up, name="sign_up")
+    path('signup', views.sign_up, name="sign_up"),
+    path('signin', views.sign_in, name="sign_in")
     # path('<int:item_id>', views.singleitem.html, name="single_item")
     # path('all/', views.items_index, name="items_index"),
 ]
