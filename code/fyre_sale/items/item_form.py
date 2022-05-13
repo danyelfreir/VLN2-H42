dfded2cdb4a905f54718898433e72dbfcdc64523
inga-dis <ingadis28@gmail.com>
2022-05-13 13:38:34 +0000

    Edit ad + calcualate rating

--- code/fyre_sale/items/item_form.py
+++ code/fyre_sale/items/item_form.py
@@ -2,18 +2,18 @@ from django import forms
 from items.models import ItemForSale, SubCategory, Offer, ItemImages
 from django.forms import ModelForm, widgets
 
-class CreateItem(ModelForm):
-    class Meta:
-        model = ItemForSale
-        fields = '__all__'
-        conditions = {
+CONDITIONS = {
             ('Refurbished', 'Refurbished'),
             ('Used - Like new', 'Used - Like new'),
             ('Used', 'Used'),
             ('For spare parts', 'For spare parts'),
         }
+class CreateItem(ModelForm):
+    class Meta:
+        model = ItemForSale
+        fields = '__all__'
         widgets = {
-            'condition': widgets.Select(attrs={'class': 'form-field'}, choices=conditions),
+            'condition': widgets.Select(attrs={'class': 'form-field'}, choices=CONDITIONS),
         }
     def __init__(self, *args, **kwargs):
         super(CreateItem, self).__init__(*args, **kwargs)
@@ -50,3 +50,26 @@ class PlaceBid(forms.ModelForm):
         if price < self.item.cur_bid:
             raise forms.ValidationError(u"Your bid must be higher than the current bid.")
         return self.cleaned_data
+
+class EditAd(ModelForm):
+    class Meta:
+        model = ItemForSale
+        fields = '__all__'
+        exclude = ['date_of_upload',
+                   'cur_bid',
+                   'sold',
+                   'id',
+                   'seller',
+                   'min_bid',
+                   ]
+        widgets = {
+            'name': widgets.TextInput(attrs={'class': 'form-field'}),
+            'image': widgets.FileInput(attrs={'class': 'form-field', 'enctype': 'multipart/form-data'}),
+            'condition': widgets.Select(attrs={'class': 'form-field'}, choices=CONDITIONS),
+            'long_desc': widgets.Textarea(attrs={'class': 'form-field'}),
+        }
+    def __init__(self, item=None, *args, **kwargs):
+        self.item = item
+        super(EditAd, self).__init__(*args, **kwargs)
+        for field in self.visible_fields():
+            field.field.widget.attrs['class'] = 'form-field'
--- code/fyre_sale/items/views.py
+++ code/fyre_sale/items/views.py
@@ -32,7 +32,7 @@ def items_index(request):
             ''' SELECT * FROM items_itemforsale I WHERE I.sub_cat_id in (
                     SELECT S.id FROM items_subcategory S WHERE S.category_id = (
                         SELECT C.id FROM items_category C WHERE C.name = %s))
-                WHERE I.sold = 'False'
+                AND I.sold = 'False'
                 ORDER BY i.date_of_upload DESC; ''', [cat]
         )
         tmp_cat = Category.objects.get(name=cat)
@@ -183,6 +183,31 @@ def decline_bid(request, offer_id):
     notify(offer_obj, offer_obj.buyer, notif_content, date_time)
     return redirect('inbox', username=request.user.username)
 
+@login_required
+def edit_ad(request, item_id):
+    chosen_item = ItemForSale.objects.get(pk=item_id)
+    if chosen_item.seller == request.user:
+        raise Http404()
+    instance = get_object_or_404(ItemForSale, pk=item_id)
+    if request.method == 'POST':
+        date = datetime.datetime.now()
+        form = EditAd(chosen_item, request.POST, instance=instance)
+        if form.is_valid():
+            item_obj = form.save(commit=False)
+            item_obj.min_bid = chosen_item.cur_bid
+            item_obj.seller_id = chosen_item.seller_id
+            item_obj.date_of_upload = date
+            item_obj.save()
+            return redirect('items_index')
+        else:
+            print(form.errors)
+    else:
+        form = EditAd()
+    return render(request, 'items/editad.html', {
+        'form': form,
+        'item': chosen_item,
+
+    })
 
 
 # ======= HELPER FUNCTIONS =========
